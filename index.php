4cc059fe4f5e30539b16284594465725834bb974
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2019-09-14 14:15:36 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,272 @@
+<?php include('header.php'); ?>
+<div class="bg-light">
+  <section class="ftco-section-featured ftco-animate">
+    <div class="container-fluid" data-scrollax-parent="true">
+      <div class="row no-gutters d-flex align-items-center" data-scrollax=" properties: { translateY: '-30%'}">
+
+        <div class="col-md-3 mb-3">
+          <a href="https://soulmedicine.io/" target="_blank" class="featured-img">
+            <div class="text-1 p-4 d-flex align-items-center">
+              <h3>Soul Medicine<br>
+                <span class="tag">Website</span></h3>
+            </div>
+            <img src="images/image_6.png" class="img-fluid" alt="">
+            <div class="text p-4 d-flex align-items-center">
+              <div class="user d-flex align-items-center">
+                <div class="user-img mr-3" style="background-image: url(http://www.tiffany-lo.net/assets/imgs/pmg.png);"></div>
+                <h3>Tiffany Lo
+                  <br>
+                  <span class="position">Front End Developer</span>
+                </h3>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-md-6">
+          <div class="row no-gutters">
+            <div class="col-md-12">
+              <div class="row no-gutters d-flex align-items-end">
+
+                <div class="col-md-8">
+                  <a href="http://gettingbetter.chayn.co" target="_blank" class="featured-img">
+                    <div class="text-1 p-4 d-flex align-items-center">
+                      <h3>Getting better & moving on guide<br>
+                        <span class="tag">Website</span></h3>
+                    </div>
+                    <img src="images/image_1.png" class="img-fluid" alt="">
+                    <div class="text p-4 d-flex align-items-center">
+                      <div class="user d-flex align-items-center">
+                        <div class="user-img mr-3" style="background-image: url(images/person_1.jpg);"></div>
+                        <h3>Tiffany Lo
+                          <br>
+                          <span class="position">Project Lead, Developer/Designer</span></h3>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+                <div class="col-md-4">
+                  <a href="https://codepen.io/tiffanylo/pen/VyNoYL" target="_blank" class="featured-img">
+                    <div class="text-1 p-4 d-flex align-items-center">
+                      <h3>I AM bassador<br>
+                        <span class="tag">Animation</span></h3>
+                    </div>
+                    <img src="images/image_4.png" class="img-fluid" alt="">
+                    <div class="text p-4 d-flex align-items-center">
+                      <div class="user d-flex align-items-center">
+                        <div class="user-img mr-3" style="background-image: url(images/person_1.jpg);"></div>
+                        <h3>John Bruce
+                          <br>
+                          <span class="position">CSS3 Animator</span></h3>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+              </div>
+            </div>
+
+            <div class="col-md-12">
+              <div class="row no-gutters d-flex align-items-start">
+                <div class="col-md-8">
+                  <a href="https://github.com/missamii/Elijah-Ink-WordPress-Theme/blob/master/README.md" target="_blank" class="featured-img">
+                    <div class="text-1 p-4 d-flex align-items-center">
+                      <h3>Curator<br>
+                        <span class="tag">Website</span></h3>
+                    </div>
+                    <img src="images/image_7.png" class="img-fluid" alt="">
+                    <div class="text p-4 d-flex align-items-center">
+                      <div class="user d-flex align-items-center">
+                        <div class="user-img mr-3" style="background-image: url(images/person_1.jpg);"></div>
+                        <h3>Tiffany Lo
+                          <br>
+                          <span class="position">Full Stack Developer</span></h3>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div class="col-md-4">
+                  <a href="https://tiffany-lo.net/folio/stjohns/" target="_blank" class="featured-img">
+                    <div class="text-1 p-4 d-flex align-items-center">
+                      <h3>Company Page - St. Johns Enterprises, Inc.<br>
+                        <span class="tag">Website</span></h3>
+                    </div>
+                    <img src="images/image_5.png" class="img-fluid" alt="">
+                    <div class="text p-4 d-flex align-items-center">
+                      <div class="user d-flex align-items-center">
+                        <div class="user-img mr-3" style="background-image: url(images/person_1.jpg);"></div>
+                        <h3>Tiffany Lo
+                          <br>
+                          <span class="position">Front Developer</span></h3>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <a href="http://elmst.surge.sh/" target="_blank" class="featured-img">
+            <div class="text-1 p-4 d-flex align-items-center">
+              <h3>WestElm<br>
+                <span class="tag">Website</span></h3>
+            </div>
+            <img src="images/image_2.png" class="img-fluid" alt="">
+            <div class="text p-4 d-flex align-items-center">
+              <div class="user d-flex align-items-center">
+                <div class="user-img mr-3" style="background-image: url(images/person_1.png);"></div>
+                <h3>Tiffany Lo
+                  <br>
+                  <span class="position">Front End Developer</span></h3>
+              </div>
+            </div>
+          </a>
+        </div>
+
+      </div>
+      <div class="row mt-5 d-flex justify-content-center">
+        <div class="col-md-8 text-center heading-section ftco-animate">
+          <h2 class="h1">UI/UX, animation,
+            <strong class="px-3">Full Stack Developer</strong>
+            with more than 5 years of experience in designing websites, mobile apps and animated stories.</h2>
+          <p>
+            <a href="mailto:tiffany_lo@icloud.com?subject=feedback" class="btn btn-primary mt-3 py-3 px-5">Get in touch</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+<!-- <div class="row justify-content-center mb-5 pb-5"> -->
+<!-- <div class="col-md-7 text-center heading-section heading-section-white ftco-animate"> -->
+
+<section class="ftco-section ftco-counter" id="section-counter">
+  <div class="container">
+    <div class="row justify-content-center mb-5 pb-5">
+      <div class="col-md-7 text-center heading-section heading-section-white ftco-animate">
+        <h2>What Exactly Am I Up To Now?</h2>
+        <p>Curious to see the lastest projects I've taken own?</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 col-lg-4 d-flex justify-content-center counter-wrap ftco-animate">
+        <div class="block-18 text-center">
+          <div class="text">
+            <!-- <strong class="number" data-number="400">0</strong> -->
+            <strong class="number">Soul Medicine</strong>
+            <span>Currently, creating a platform with
+              <a href="http://chayn.co/" target="_blank">CHAYN</a>
+              to empower victims and surivors through helpful online guides. We're looking forward to launching this project in late Summer of 2019.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 d-flex justify-content-center counter-wrap ftco-animate">
+        <div class="block-18 text-center">
+          <div class="text">
+            <!-- <strong class="number" data-number="1000">0</strong> -->
+            <strong class="number">Chayn: Mental Health Guide
+            </strong>
+            <span>In May '19, I help to complete a project that started almost a year ago. I leveraged my skills and picked up new ones on the fly. As I learned and taught Webflow to a UX team who hadn't or had little experience with desingin and building for
+              users before. If you'd like to listen to how it was done check out our
+              <a href="#" target="_blank">Podcast</a>
+              or if you'd prefer to read, you can find the
+              <a href="#" target="-_blank">blog here</a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 d-flex justify-content-center counter-wrap ftco-animate">
+        <div class="block-18 text-center">
+          <div class="text">
+            <!-- <strong class="number" data-number="8000">0</strong> -->
+            <strong class="number">Are You Hiring?</strong>
+            <span>I am currently open to new remote oppurtunities. Would love to connect with companies making an impact leveraging tech. I am open to freelancing as well. :)</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="ftco-section bg-light">
+  <div class="container">
+    <div class="row justify-content-center mb-5 pb-5">
+      <div class="col-md-7 text-center heading-section ftco-animate">
+        <span class="subheading">Blog</span>
+        <h2>Recent Blog</h2>
+        <p>Tech to life adventures. New friends, meet ups and hilarous stories. Read along to some of my most recent and old blogs.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 ftco-animate">
+        <div class="blog-entry">
+          <a href="blog-single.html" class="block-20" style="background-image: url('images/image_6.png');"></a>
+          <div class="text p-4 d-block">
+            <div class="meta mb-3">
+              <div>
+                <a href="#">July 12, 2019</a>
+              </div>
+              <div>
+                <a href="#">Tiffany</a>
+              </div>
+              <div>
+                <!-- <a href="#" class="meta-chat"> <span class="icon-chat"></span> 3</a> -->
+              </div>
+            </div>
+            <h3 class="heading">
+              <a href="#">Even the all-powerful Pointing has no control about the blind texts</a>
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 ftco-animate">
+        <div class="blog-entry" data-aos-delay="100">
+          <a href="blog-single.html" class="block-20" style="background-image: url('images/image_1.png');"></a>
+          <div class="text p-4">
+            <div class="meta mb-3">
+              <div>
+                <a href="#">July 12, 2019</a>
+              </div>
+              <div>
+                <a href="#">Tiffany</a>
+              </div>
+              <div>
+                <!-- <a href="#" class="meta-chat"> <span class="icon-chat"></span> 3</a> -->
+              </div>
+            </div>
+            <h3 class="heading">
+              <a href="#">Even the all-powerful Pointing has no control about the blind texts</a>
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 ftco-animate">
+        <div class="blog-entry" data-aos-delay="200">
+          <a href="blog-single.html" class="block-20" style="background-image: url('images/image_3.jpg');"></a>
+          <div class="text p-4">
+            <div class="meta mb-3">
+              <div>
+                <a href="#">July 12, 2019</a>
+              </div>
+              <div>
+                <a href="#">Tiffany</a>
+              </div>
+              <div>
+                <!-- <a href="#" class="meta-chat"> <span class="icon-chat"></span> 3</a> -->
+              </div>
+            </div>
+            <h3 class="heading">
+              <a href="#">Back to React JS</a>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php include('footer.php'); ?>
